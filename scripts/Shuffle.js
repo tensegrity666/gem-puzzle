5dde1608e3d5387f908dfb330daf25640420e8a2
Roman Yakovlev <tensegrity666@eicloud.com>
2020-04-07 23:57:50 +0300

    feat: implement check for win

--- scripts/Shuffle.js
+++ scripts/Shuffle.js
@@ -1,3 +1,5 @@
+// shuffles an array of initial values of the playing field
+
 export default function shuffle(array) {
   const newArray = array.sort(() => Math.random() - 0.5);
   return newArray;
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,11 @@
 import { pageInit } from './PageInit.js';
 import startTimer from './Timer.js';
+import checkWin from './CheckWin.js';
+import replaceItem from './ReplaceItem.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   pageInit();
   startTimer();
+  checkWin();
+  replaceItem();
 });
